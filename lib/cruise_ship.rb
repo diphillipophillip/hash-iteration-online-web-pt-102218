4c2f4ce5b3b8e829708c609127ddb5c303c87b1b
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-11-11 04:23:44 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -17,3 +17,9 @@ def select_winner(passengers)
 end 
 
 
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!" 
+  end 
+end 
+
